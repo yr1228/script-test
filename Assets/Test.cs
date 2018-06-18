ab04af76e01dc81a62ab869d353de89428d31feb
ナカムラユリ <lizard_1228_ray@outlook.jp>
2018-06-18 22:33:04 +0900

    c#スクリプト基礎課題
    配列を宣言して出力しました。

---
+++ Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        int[] array = { 12, 28, 34, 23, 45 };
+
+        for (int i = 0; i < 5; i++) {
+
+            Debug.Log(array[i]);
+        }
+
+        for (int i = array.Length - 1; 0 <= i; i--){
+
+            Debug.Log(array[i]);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
