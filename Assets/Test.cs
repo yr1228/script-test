ffab39de4af3e4609aa98da4575a77fb5295521c
ナカムラユリ <lizard_1228_ray@outlook.jp>
2018-06-19 22:11:53 +0900

    Lesson4 発展課題
    Lesson4の課題に発展課題の内容を追加しました。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss{
+
+    int mp = 53;
+
+    public void Magic(int a){
+
+        Debug.Log("魔法攻撃をした。残りMPは" + a + "。");
+        this.mp -= a;
+
+        if (mp < 5){
+
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+}
+
 public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+        Boss lastboss = new Boss();
+
+        lastboss.Magic(5);
+        for(int i = 0; i < 10; i++){
+
+            lastboss.Magic(5);
+        }
+
 
         int[] array = { 12, 28, 34, 23, 45 };
 
@@ -18,6 +43,10 @@ public class Test : MonoBehaviour {
 
             Debug.Log(array[i]);
         }
+
+        
+
+        
     }
 	
 	// Update is called once per frame
